a08d3c55a12ffc7808f1510345bb2c3ae0d4dd43
Javiert JAAR <javierthsm@gmail.com>
2017-08-09 01:08:45 -0400

    Update core.js

--- js/core.js
+++ js/core.js
@@ -1,15 +1,1 @@
-var qs =
-document.getElementById("identificador_js").src.match(/\w+=\w+/g);
- 
-var _GET= {};
- 
-var t,i = qs.length;
-while (i--) {
- 
-     t = qs[i].split("=");
- 
-     _GET[t[0]] = t[1];
- 
-    eval('var '+t[0]+'="'+t[1]+'";');
-}
-document.getElementById("player").innerHTML = (_GET['nombre'] + " " + nombre);
+document.getElementById("player").innerHTML = "JaviertINC";
