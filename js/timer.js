c56060d774f47b29fd27d4c1acfc60f0021d6d1b
Javiert JAAR <javierthsm@gmail.com>
2017-08-17 12:52:48 -0300

    Update timer.js

--- js/timer.js
+++ js/timer.js
@@ -6,7 +6,7 @@ var x = setInterval(function() {
     var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
     var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
     var seconds = Math.floor((distance % (1000 * 60)) / 1000);
-    document.getElementById("demo").innerHTML = days + "d " + hours + "h "
+    document.getElementById("timerq").innerHTML = days + "d " + hours + "h "
     + minutes + "m " + seconds + "s ";
     if (distance < 0) {
         clearInterval(x);
