497a0cba7930be4736e364a942a72a0f761e296e
Javiert JAAR <javierthsm@gmail.com>
2017-08-17 09:10:09 -0300

    Update core.js

--- js/core.js
+++ js/core.js
@@ -7,12 +7,3 @@ $(document).ready(function(){
         $("#player").text(currentText);
     });
 });
-$(document).ready(function(){
-    $("#mycur").keyup(function(){
-        // Getting the current value of textarea
-        var currentText = $(this).val();
-        
-        // Setting the Div content
-        $("#cursor").text(currentText);
-    });
-});
