992e5079145576499371a9e93d24ddd20164d279
Javiert JAAR <javierthsm@gmail.com>
2017-08-17 08:42:19 -0300

    Update core.js

--- js/core.js
+++ js/core.js
@@ -3,23 +3,23 @@ window.onload = function() {
 		gethead = document.getElementById("myid"),
         head = document.getElementById("face"),
         complete = document.getElementById("body"),
-        test = document.getElementById("test");
+        test = document.getElementById("player");
 		
 
     elm.onkeyup = function(event) {
-	document.getElementById("player").innerHTML = '/give @p minecraft:skull 1 3 {SkullOwner:"'+document.getElementById('myid').value +'"}';
+	document.getElementById("player").innerHTML = document.getElementById('username').value +' '+ window.location.href;
     if (elm.value == null || elm.value == "") {
             document.getElementById("player").innerHTML = "Ingresa un Nombre!";
     } else {
      head.src = "https://crafatar.com/avatars/" + document.getElementById('myid').value + "?default=steve";
-      document.getElementById("test").innerHTML = elm.value;
+      document.getElementById("player").innerHTML = elm.value;
     } 
     if (elm.value == null || elm.value == "") {
       complete.src = "https://crafatar.com/renders/body/steve?helm&scale=4";
-            document.getElementById("test").innerHTML = "Ingresa un Nick";
+            document.getElementById("player").innerHTML = "Ingresa un Nick";
     } else {
      complete.src = "https://crafatar.com/renders/body/" + document.getElementById('myid').value + "?helm&scale=4";
-      document.getElementById("test").innerHTML = elm.value;
+      document.getElementById("player").innerHTML = elm.value;
     }
 }
 }
